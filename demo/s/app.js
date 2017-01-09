221fd5f3ede4295491ad161f1fb622823fd200c6
arivin <btc022003@gmail.com>
2017-01-09 19:33:10 +0800

    nodejs从网络下载指定文件

---
+++ demo/s/app.js
@@ -0,0 +1,12 @@
+var imageLinks = [
+    'http://i1.umei.cc/uploads/tu/201701/28/4jsoidd1t0z.jpg',
+    'http://i1.umei.cc/uploads/tu/201701/28/tojckcrytxv.jpg'
+]
+var imageLinks2 = [
+    'http://i1.umei.cc/uploads/tu/201701/28/c30fwbuy5wq.jpg',
+    'http://i1.umei.cc/uploads/tu/201701/28/jamvsc34w1b.jpg'
+]
+var d = require('./download')
+console.log(d)
+d.download(imageLinks)
+d.download(imageLinks2)
